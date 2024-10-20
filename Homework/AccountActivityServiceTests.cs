abb88ea555513d2f773628307d32cd8b432d623e
Asdasdone <tothm8@stud.uni-obuda.hu>
2024-10-20 12:34:09 +0200

    Add project files.

---
+++ Homework/AccountActivityServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using Homework;
+using Homework.ThirdParty;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+[TestFixture]
+public class AccountActivityServiceTests
+{
+    private Mock<IAccountRepository> _accountRepositoryMock;
+    private AccountActivityService _service;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _accountRepositoryMock = new Mock<IAccountRepository>();
+        _service = new AccountActivityService(_accountRepositoryMock.Object);
+    }
+
+    [Test]
+    public void GetActivity_AccountExists_ReturnsCorrectActivityLevel()
+    {
+        var account = new Account(1, 25);
+        _accountRepositoryMock.Setup(r => r.Get(It.IsAny<int>())).Returns(account);
+
+        ActivityLevel result = _service.GetActivity(1);
+
+        Assert.That(result, Is.EqualTo(ActivityLevel.Medium));
+    }
+
+    [Test]
+    public void GetActivity_AccountDoesNotExist_ThrowsException()
+    {
+        _accountRepositoryMock.Setup(r => r.Get(It.IsAny<int>())).Returns<Account>(null);
+
+        Assert.Throws<AccountNotExistsException>(() => _service.GetActivity(1));
+    }
+
+    [Test]
+    public void GetAmountForActivity_ActivityLevelLow_ReturnsCorrectCount()
+    {
+
+        var accounts = new List<Account>
+    {
+        new Account(1,15),
+        new Account(2,30),
+        new Account(3,5) 
+    };
+
+        _accountRepositoryMock.Setup(r => r.GetAll()).Returns(accounts.AsQueryable());
+
+        foreach (var account in accounts)
+        {
+            _accountRepositoryMock.Setup(r => r.Get(account.Id)).Returns(account);
+        }
+
+        int result = _service.GetAmountForActivity(ActivityLevel.Low);
+
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
---
+++ Homework/AccountTests.cs
@@ -0,0 +1,55 @@
+﻿using Homework;
+using Homework.ThirdParty;
+using Moq;
+using NUnit.Framework;
+
+[TestFixture]
+public class AccountTests
+{
+    private Mock<IAction> _actionMock;
+    private Account _account;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _actionMock = new Mock<IAction>();
+        _account = new Account(1);
+    }
+
+    [Test]
+    public void Register_AccountNotRegistered_SetsIsRegisteredToTrue()
+    {
+        _account.Register();
+
+        Assert.That(_account.IsRegistered, Is.True);
+    }
+
+    [Test]
+    public void Activate_AccountNotConfirmed_SetsIsConfirmedToTrue()
+    {
+        _account.Activate();
+
+        Assert.That(_account.IsConfirmed, Is.True);
+    }
+
+    [Test]
+    public void TakeAction_AccountActive_ActionExecutedSuccessfully_IncrementsActionsSuccessfullyPerformed()
+    {
+        _actionMock.Setup(a => a.Execute()).Returns(true);
+        _account.Register();
+        _account.Activate();
+
+        bool result = _account.TakeAction(_actionMock.Object);
+
+        Assert.That(result, Is.True);
+        Assert.That(_account.ActionsSuccessfullyPerformed, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TakeAction_AccountInactive_ThrowsException()
+    {
+        _actionMock.Setup(a => a.Execute()).Returns(true);
+
+        Assert.Throws<InactiveUserException>(() => _account.TakeAction(_actionMock.Object));
+    }
+}
